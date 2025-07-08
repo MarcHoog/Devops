fe3dd533872fc3156fd2df6237ca40c4941913fd
Marc Hoogendoorn <marc.hoogendoorn@live.nl>
2025-07-08 22:59:48 +0200

    updated some stuff

---
+++ scripts/create_hetzner_snapshot.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+import os, json, subprocess, argparse
+from typing import Dict
+
+def lookup_snapshots() -> Dict:
+    out = subprocess.check_output([
+        "hcloud", "image", "list", "--selector", "type=snapshot", "--output", "json"
+    ])
+    images = json.loads(out)
+    return images
+
+def delete_snapshot(snapshot_id) -> bool:
+    try:
+        subprocess.check_output([
+            "hcloud", "image", "delete", snapshot_id
+        ]
+    )
+        return True
+    except subprocess.CalledProcessError as e:
+        print("Exit code:", e.returncode)
+        return False
+
+def create_snapshot(machine: Dict) -> bool:
+    try:
+        subprocess.check_output([
+            "hcloud", 
+            "server", 
+            "create-image",
+            "--type",
+            "snapshot", 
+            "--description",
+            f"vm-snapshot-{machine['name']}",
+            machine["id"]
+        ])
+    except subprocess.CalledProcessError as e:
+        print("Exit code:", e.returncode)
+        return False
+
+    return True
+
+
+def lookup_machines() -> Dict:
+    out = subprocess.check_output([
+        "hcloud", "server", "list" "--output" "json " 
+    ])
+    machines = json.loads(out)
+    return machines
+
+
+
+def main():
+    snapshots = lookup_snapshots()
+    snapshot_descriptions = {x.get('description', "").removeprefix("vm-snapshot-"): x['id'] for x in snapshots}
+    machines = lookup_machines()
+
+    for m in machines:
+        name = m["name"]
+        if name in snapshot_descriptions:
+            print("Deleting existing Snapshot of Machine ")
+            deleted = delete_snapshot(snapshot_descriptions[name])
+            if not deleted:
+                print("Couldn't delete snapshot so skipping creating a new one")
+                continue
+
+        created = create_snapshot(m)
+        if not created:
+            print("Something went wrong creating a new Image")
+
+        
+
+
+
+
