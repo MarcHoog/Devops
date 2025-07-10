23621ed9a576a6983d0f66b9e7e460802be78d85
Marc Hoogendoorn <marc.hoogendoorn@live.nl>
2025-07-10 21:06:00 +0200

    updated stuff

--- terraform/env/hetzner/babilon/main.tf
+++ terraform/env/hetzner/babilon/main.tf
@@ -15,7 +15,7 @@ terraform {
 
 module "singleNodeK3s" {
   source       = "git::https://github.com/MarcHoog/devops.git//terraform/mod/hetzner/server?ref=main"
-  server_name = ""
+  server_name = "boeing747"
   image        = "ubuntu-24.04"
   server_type  = "cx22"
   location     = "nbg1"
