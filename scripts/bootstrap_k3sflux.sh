6a44f91f76813fd624848f53e478dc77dff37a39
Zamyza <58691229+MarcHoog@users.noreply.github.com>
2025-08-11 10:50:52 +0200

    Delete scripts/bootstrap_k3sflux.sh

--- scripts/bootstrap_k3sflux.sh
+++
@@ -1,8 +0,0 @@
-TMP_SCRIPT=$(mktemp /tmp/bootstrap_flux.XXXXXX.py)
-cleanup() {
-  rm -f "$TMP_SCRIPT"
-}
-trap cleanup EXIT INT TERM
-curl -s -o "$TMP_SCRIPT" https://raw.githubusercontent.com/MarcHoog/Devops/main/scripts/bootstrap_k3sflux.py
-chmod +x "$TMP_SCRIPT"
-python3 "$TMP_SCRIPT"
