d4c51766f9ba872d6f11a48af68b4aefede8855f
Marc Hoogendoorn <marc.hoogendoorn@live.nl>
2025-09-02 18:21:26 +0200

    updated

--- tf-environments/schiphol/main.tf
+++ tf-environments/schiphol/main.tf
@@ -23,7 +23,7 @@ module "TalosNode1" {
   source       = "git::https://github.com/MarcHoog/devops.git//tf-building-blocks/hetzner/server?ref=main"
 
   server_name  = "boeing-737"
-  image        = "312961706" 
+  image        = "ubuntu-24.04" 
   server_type  = "cx32"
   location     = "nbg1"
   ssh_keys     = ["bubble", "ansible"]
@@ -31,30 +31,19 @@ module "TalosNode1" {
 
   # Labels
   labels = {
-    "k3s" : "controller"
-    "k3s" : "flux"
     "arch" : "x86"
   }
 
-  # Firewall
   enable_firewall = true
   firewall_name   = "boeing-737-fw"
   firewall_rules = {
     ssh_in = {
       direction   = "in"
       protocol    = "tcp"
-      port        = "6443"
+      port        = "22"
       ips         = ["0.0.0.0/0", "::/0"]
       description = "Allow SSH"
     }
-    talos_management = {
-      direction = "in"
-      protocol = "tcp"
-      port = "50000"
-      ips = ["91.180.39.10"]
-      description = "Allow Talos Management plane"
-    }
-
     icmp_in = {
       direction = "in"
       protocol  = "icmp"
