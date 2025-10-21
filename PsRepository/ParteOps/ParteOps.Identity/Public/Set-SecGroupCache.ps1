f1086bfc788c0570e690963debcf625db414d3d1
Marc Hoogendoorn <marc.hoogendoorn@live.nl>
2025-10-21 22:00:51 +0200

    Clean up

--- PsRepository/ParteOps/ParteOps.Identity/Public/Set-SecGroupCache.ps1
+++
@@ -1,5 +0,0 @@
-function Update-SecGroupCompleter {
-    $scopes = @("Group.Read.All", "Group.ReadWrite.All", "Directory.Read.All", "User.Read.All")
-    Connect-MgGraph -Scopes $scopes -NoWelcome
-    $script:SecGroupCache = Get-MgGroup -All | Where-Object { $_.GroupTypes -notcontains "Unified" }
-}
